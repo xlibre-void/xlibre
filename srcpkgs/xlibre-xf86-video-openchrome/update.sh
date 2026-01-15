8c34504765d1b332343f2d9f05ed5e31bbe03a71
sofijacom <sofija.p2018@gmail.com>
2026-01-15 16:59:20 +0300

    Update

--- srcpkgs/xlibre-xf86-video-openchrome/update.sh
+++ srcpkgs/xlibre-xf86-video-openchrome/update.sh
@@ -5,7 +5,7 @@ printf "Checking latest version\n"
 __dir="$(dirname "${BASH_SOURCE[0]}")"
 
 LATEST_VERSION=$(gh release list --repo X11Libre/xf86-video-openchrome --json name,tagName,isLatest --jq '.[] | select(.isLatest)|.tagName')
-export VERSION=${LATEST_VERSION#"xlibre-xf86-video-r128-"}
+export VERSION=${LATEST_VERSION#"xlibre-xf86-video-openchrome-"}
 CURRENT_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)
 
 printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CURRENT_VERSION}"
