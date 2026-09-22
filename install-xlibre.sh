371a1620e622407a03a46a08fcfab1010aed167d
Sofiya <sofija.p2018@gmail.com>
2026-09-23 01:48:54 +0300

    Update install-xlibre.sh
    Signed-off-by: Sofiya <sofija.p2018@gmail.com>

--- install-xlibre.sh
+++ install-xlibre.sh
@@ -21,7 +21,7 @@
 mkdir -p /etc/xbps.d
 # add Xlibe github mirror repository-x86_64-glibc
 cat <<'SETMIRROR' > /etc/xbps.d/99-repository-xlibre.conf
-repository=https://github.com/xlibre-void/xlibre/releases/latest/download/
+repository=https://github.com/xlibre-void/xlibre/releases/latest/download
 SETMIRROR
 # Add a public key to sign the repository
 # sudo wget -O /var/db/xbps/keys/00:ca:42:57:c9:c0:9a:ec:94:b4:7d:97:e5:a9:aa:1e.plist https://github.com/xlibre-void/xlibre/raw/refs/heads/main/repo-keys/x86_64/00:ca:42:57:c9:c0:9a:ec:94:b4:7d:97:e5:a9:aa:1e.plist
